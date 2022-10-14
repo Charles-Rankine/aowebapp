8916a312c74a013a6f0be2c722cc47e0dd905a97
Charles Rankine <rancd001@mymail.unisa.edu.au>
2022-10-15 00:49:09 +1030

    Added Category Search (buggy)

--- src/components/CardListSearch.js
+++ src/components/CardListSearch.js
@@ -21,28 +21,67 @@ const CardListSearch = () => {
     const searchQuery = () => {
         // const value = document.querySelector("[name='searchText']").value; // Element by Query Selector
         const search = document.getElementsByName("searchText")[0].value;      // Element by Name
-        const category = document.getElementsByName("Category")[0].value;
-
-        setFiltered(allCards.filter((item) => {
-            return item.itemName.toLowerCase()
-                .indexOf(search.toLowerCase()) !== -1; 
-        }))
+        const categoryId = getCategoryId(document.getElementsByName("Category")[0].value);
         
-        setFiltered(filteredCards.filter((item) => {
-            return item.itemCategory
-                .indexOf(category) !== -1;
-        }))
+        setFiltered(allCards);
 
-        if (filteredCards.length === 0) { setFiltered(allCards) }
+        if (search !== "") 
+        {
+            setFiltered(allCards.filter((item) => 
+            {
+                return item.itemName.toLowerCase()
+                    .indexOf(search.toLowerCase()) !== -1;
+            }))
+        }
+
+        if (categoryId !== undefined)
+        {
+            setFiltered(filteredCards.filter((item) => 
+            {
+                 return item.categoryId === categoryId; 
+            }))
+        }
+
+        console.log(`Search: ${search} || CategoryId: ${categoryId}`);
+        filteredCards.map((p) => (console.log(`ItemName: ${p.itemName} (${p.itemName.toLowerCase().indexOf(search.toLowerCase()) !== -1})|| ItemCategory: ${p.categoryId} (${p.categoryId === categoryId})`)))
+
+        if (search === "" && categoryId === undefined) 
+        {
+            console.log("RESET");
+            setFiltered(allCards) 
+        }
+    }
+
+    const getCategoryId = (categoryString) => {
+        for(var i = 0; i < categoryList.length; i++) {
+             if (categoryList[i].categoryName === categoryString) {
+                return categoryList[i].categoryId;
+             }
+        }
+    }
+    
+    const getCategoryNames = (categories) => {
+        var names = [];
+
+        for(var i = 0; i < categories.length; i++) {
+            names.push(categories[i].categoryName);
+        }
+
+        return names
     }
 
     return (
         <div id="cardListSearch">
-        <select name="Category" for="Category" class="col">
-            <option value="">All categories...</option>
-            
-
+        <select name="Category" htmlFor="Category" className="col">
+        <option value="">All categories...</option>
+        {
+            getCategoryNames(categoryList).map((cat) => 
+            (
+                <option key={cat} value={cat}>{cat}</option>
+            ))
+        }
         </select>
+
             <div className="row justify-content-center mb-3 pt-2">
                 <div className="col-3">
                     <input type="text" name="searchText" className="form-control" placeholder="Search..." />
