f036bdc11dfb645d24077a3dda6306fb7d2ca3be
Charles Rankine <charlie.rankine@gmail.com>
2022-10-20 12:37:49 +1030

    Changed some styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     <div className="App container">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/Home">Navbar</a>
+          <a className="navbar-brand" href="/Home">Amazon Items</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -17,26 +17,35 @@ const CardDetail = () => {
     }, [itemId])
 
     return (
-        <div className="col-3 mb-2 px-2">
-            <div className="card" style={{backgroundColor: "lightgray"}}>
-                <div className="card-body">
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <img className="card-img-top" src={cardData.itemImage} alt={'Image of ' + cardData.itemName} />
-                        </li>
-
-                        <li className="list-group-item"><h5>{cardData.itemName}</h5></li>
-                        <li className="list-group-item">{cardData.itemDescription}</li>
-                        <li className="list-group-item">Price: ${cardData.itemCost}</li>
-
-                        <li className="list-group-item">
-                            <Link to={'/Products/'} className='btn btn-primary'>Back to products</Link>
-                        </li>
+        <div className='row'>
+            <div className="col-3 mb-2 px-2">
+                    <div className="card-body">
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">
+                                <img className="card-img-top" src={cardData.itemImage} alt={'Image of ' + cardData.itemName} />
+                            </li>
 
                         </ul>
                 </div>
             </div>
+            <div className="col-9 mb-2 px-2">
+                <div className='card'>
+                    <div className='card-body'>
+                        <ul className='list-group list-group-flush'>
+                                <li className="list-group-item"><h1>{cardData.itemName}</h1></li>
+                                <li className="list-group-item"><h3>{cardData.itemDescription}</h3></li>
+                                <li className="list-group-item"><h3>Price: ${cardData.itemCost}</h3></li>
+
+                                <li className="list-group-item">
+                                    <Link to={'/Products/'} className='btn btn-primary'>Back to products</Link>
+                                </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
     )
 }
 
